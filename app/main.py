382c910ea5cbf858494ddab98c83d3a4ef10a5ad
Volodimir <vova.lox1337@gmail.com>
2024-11-18 20:20:36 +0200

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,7 +1,5 @@
-# Импорты должны находиться на самом верху файла (исправляем E402)
+# Импорты должны находиться на самом верху файла
 from typing import List, Dict
-
-# Добавляем две пустые строки перед началом функции (исправляем E302)
 
 
 def format_linter_error(error: dict) -> dict:
@@ -16,7 +14,7 @@ def format_linter_error(error: dict) -> dict:
 
 def format_single_linter_file(file_path: str, errors: List[dict]) -> dict:
     return {
-        "errors": [  # Формируем список ошибок прямо в возвращаемом словаре
+        "errors": [
             {
                 "line": error["line_number"],
                 "column": error["column_number"],
@@ -29,7 +27,6 @@ def format_single_linter_file(file_path: str, errors: List[dict]) -> dict:
         "path": file_path,
         "status": "failed" if errors else "passed",
     }
-
 
 
 def format_linter_report(linter_report: Dict[str, List[dict]]) -> List[dict]:
